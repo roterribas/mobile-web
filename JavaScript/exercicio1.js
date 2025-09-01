3b19e3f94ac945e578e9113b355b5f26ec340cab
roterribas <roterribas@uol.com.br>
2025-09-01 16:19:24 -0300

    adicionando o arquivo de primeiro exercicio onde pega algumas letras de uma variavel

---
+++ JavaScript/exercicio1.js
@@ -0,0 +1,12 @@
+let nome = "Rodrigo Terribas Saraiva";
+let letras = nome[0] + nome[8] + nome[17];
+
+let programacao = "JavaScript"
+let letras2 = programacao[0] + programacao[4];
+
+/* JavaScript
+    JS
+*/
+
+console.log(letras);
+console.log(letras2);
