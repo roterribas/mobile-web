d6b71da3ca33cf6a44095b4ab57cb114486abdab
roterribas <roterribas@uol.com.br>
2025-09-01 16:03:01 -0300

    adicionando o arquivo  de tipos de dados

---
+++ JavaScript/tipos-dados.js
@@ -0,0 +1,22 @@
+// string
+let nome = "Rodrigo";
+
+// inteiro
+let idade = 34;
+
+// boleanno
+let casado = true;
+
+// numeros flueantes com casas decimais
+let valorProduto = 5.35;
+
+
+// resultado
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: ", idade);
+console.log("Casado?", casado);
+console.log("R$", valorProduto);
+console.log(typeof idade);
+console.log(typeof nome);
+console.log(typeof casado);
+console.log(typeof valorProduto);
