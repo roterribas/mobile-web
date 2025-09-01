e72b765e1f6a1d718a36edb7d3f7449c20da3fdb
roterribas <roterribas@uol.com.br>
2025-09-01 15:50:55 -0300

    criando um arquivo de variais do tipo inteiro e string

---
+++ JavaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "Olá, mundo!";
+
+const nomeCompleto = "Rodrigo Terribas Saraiva";
+
+let idade = 34;
+
+// resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + ", minha idade é: " + idade + " anos");
