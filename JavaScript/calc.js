884e4a64084d8f9d399640f877eaa2504257d305
roterribas <roterribas@uol.com.br>
2025-09-02 14:48:06 -0300

    adicionado exercicio dois e uma calculadora

---
+++ JavaScript/calc.js
@@ -0,0 +1,18 @@
+// duas variaveis
+let num1 = 20;
+let num2 = 10;
+
+// operações
+let soma = num1 + num2
+
+// imprimir as respostas
+console.log("SOMA: ", soma);
+
+let sub = num1 - num2;
+console.log("SUBTRAÇÃO: ", sub);
+
+let mult = num1 * num2;
+console.log("MULTIPLICAÇÃO: ", mult);
+
+let divisao = num1 / num2;
+console.log("DIVISÃO: ", divisao);
---
+++ JavaScript/exercicio2.js
@@ -0,0 +1,8 @@
+/* 
+    exiba no console
+    a quantidade de caracteres que o const possui
+*/
+
+const linguages = "JavaScript";
+
+console.log(linguages.length);
