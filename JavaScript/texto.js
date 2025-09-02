cebd7e28bc6622cd146d7ceae75e8ca840638696
roterribas <roterribas@uol.com.br>
2025-09-02 14:25:00 -0300

    adicionando arquivo de funçoes nativas do js

---
+++ JavaScript/texto.js
@@ -0,0 +1,16 @@
+let text = "Estão chegando as provas";
+
+// tamanho da string
+console.log(text.length)
+
+// replace
+console.log(text.replace("provas", "avaliações"));
+
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+
+// retorna a primeira posição da string
+console.log(text.indexOf("as"));
+
+// retorna a ultima posição da string
+console.log(text.lastIndexOf("as"));
