f5c5e1c41e8362b27c616e4ece8090948d6a06ba
roterribas <roterribas@uol.com.br>
2025-09-03 16:12:33 -0300

    mexendo no arquivo calNew

---
+++ JavaScript/calcNew.js
@@ -0,0 +1,45 @@
+// duas variaveis
+// let num1 = 10;
+// let num2 = 10;
+
+// funções
+function somar(iden, num1, num2) {
+    console.log(iden, num1 + num2);
+}
+
+function sub(iden, num1, num2) {
+    console.log(iden, num1 - num2);
+}
+
+function mult(iden, num1, num2) {
+    console.log(iden, num1 * num2);
+}
+
+function div(iden, num1, num2) {
+    console.log(iden, num1 / num2);
+}
+
+// imprimir funçoes
+somar("SOMA", 10, 10); // 10 + 10
+somar("SOMA", 10, 20); // 10 + 20
+somar("SOMA", 10, 15);
+somar("SOMA", 3, 3);
+somar("SOMA", 4, 4);
+
+sub("SUB", 10, 10); // 10 + 10
+sub("SUB", 10, 20); // 10 + 20
+sub("SUB", 10, 15);
+sub("SUB", 3, 3);
+sub("SUB", 4, 4);
+
+mult("MULT", 10, 10); // 10 + 10
+mult("MULT", 10, 20); // 10 + 20
+mult("MULT", 10, 15);
+mult("MULT", 3, 3);
+mult("MULT", 4, 4);
+
+div("DIV", 10, 10); // 10 + 10
+div("DIV", 10, 20); // 10 + 20
+div("DIV", 10, 15);
+div("DIV", 3, 3);
+div("DIV", 4, 4);
