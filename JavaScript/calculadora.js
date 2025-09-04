f50d9997ab1dfbf33347ca57fa47cbb442e8f8df
roterribas <roterribas@uol.com.br>
2025-09-04 16:17:19 -0300

    feito uma calculadora, onde o usuario digitas o numero e clica no botão para ver o resultado da equação

--- JavaScript/calculadora.js
+++ JavaScript/calculadora.js
@@ -1,3 +1,33 @@
 // variavel
 const num1 = document.getElementById("numero1");
 const num2 = document.getElementById("numero2");
+const resultado = document.getElementById("resultado");
+
+// funçoes
+function soma(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) + Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
+
+function sub(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) - Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
+
+function mult(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) * Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
+
+function div(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) / Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
