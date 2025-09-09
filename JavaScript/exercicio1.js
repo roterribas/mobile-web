2f56c9a6a0ca12147482763e5868599d7692e74b
roterribas <roterribas@uol.com.br>
2025-09-09 15:40:47 -0300

    criação do arquivo for e exercicio 1

---
+++ JavaScript/exercicio1.js
@@ -0,0 +1,14 @@
+/*
+  - Escreva um loop (for) que exiba a frase abaixo
+   no console;
+  - Substitua o "X" pela informação correta;
+  - "X" deve começar em 1 na 1ª exibição da frase e em 
+  10 na última;
+  - Exiba a frase 10x no console.
+
+  "Esta é a Xª vez que esta frase é exibida no console."
+*/
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Esta é a ${i}ª vez que esta frase é exibida no console.`);
+}
