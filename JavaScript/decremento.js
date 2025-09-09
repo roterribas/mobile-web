8b4aa347a6bd185b139d9e7cab85d7ec6da98a78
roterribas <roterribas@uol.com.br>
2025-09-09 14:37:50 -0300

    criado os arquivos incremento e decremento

---
+++ JavaScript/decremento.js
@@ -0,0 +1,19 @@
+let valor = 7;
+
+valor = valor - 1;
+
+console.log(valor);
+
+valor--;
+
+console.log(valor);
+
+// multiplicação
+valor *= 2;
+
+console.log(valor);
+
+// divisão
+valor /= 4;
+
+console.log(valor);
---
+++ JavaScript/incremento.js
@@ -0,0 +1,15 @@
+let valor = 7;
+
+console.log(valor);
+
+valor = valor + 1;
+
+console.log(valor);
+
+valor++;
+valor++;
+console.log(valor);
+
+valor += 3;
+
+console.log(valor);
