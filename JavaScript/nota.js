34f084baaca2d6319d28b438d1b6c824ced39807
roterribas <roterribas@uol.com.br>
2025-09-10 16:06:12 -0300

    criado a função para mostrar se aluno esta reprovado acimaigual de 6 ou reprovado

--- JavaScript/nota.js
+++ JavaScript/nota.js
@@ -3,6 +3,16 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
 // função
-function resultadoNotaClick() {
-    alert("Resultado");
+function resultadoNotaClick(event) {
+    event.preventDefault();
+    // console.log(inputNota.value);
+    // logica - condição
+    if (inputNota.value >= 6) {
+        resultado.innerHTML = "APROVADO ✅";
+    } else {
+        resultado.innerHTML = "REPROVADO ❌";
+    }
+
+    inputNota.value = ""
+
 }
