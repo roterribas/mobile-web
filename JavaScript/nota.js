dfbd863514a921fb575ce25e20567ed6ddcc1efb
roterribas <roterribas@uol.com.br>
2025-09-11 14:40:21 -0300

    colocado um alerta na tela para o usuario digitar anota caso ele click no botão sem colocar um numero

--- JavaScript/nota.js
+++ JavaScript/nota.js
@@ -1,12 +1,20 @@
 // variavies
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
+const message = document.getElementById("message")
 
 // função
 function resultadoNotaClick(event) {
     event.preventDefault();
     // console.log(inputNota.value);
     // logica - condição
+     if (inputNota.value == "") {
+        message.style.display = "block";
+        return false;
+    }
+
+        message.style.display = "none";
+    
     if (inputNota.value >= 6) {
         resultado.innerHTML = "APROVADO ✅";
     } else {
