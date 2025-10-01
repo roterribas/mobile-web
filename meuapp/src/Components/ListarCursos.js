cb42412e81a63b0c98b35a8f443d753dad690f71
roterribas <roterribas@uol.com.br>
2025-10-01 16:15:58 -0300

    stilizando o ListarCursos.js

--- meuapp/src/Components/ListarCursos.js
+++ meuapp/src/Components/ListarCursos.js
@@ -2,19 +2,44 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 
 export default function ListarCursos() {
+  const data = ["React Native", "Mongo DB", "Express", "Node.js", "C#"]
   return (
-    <View style={estuilos.container}>
+    <View style={estilos.container}>
         <FlatList
-          data={["React Native", "Node.js", "MongoDB", "HTML Popeto"]}
-          renderItem={({ item }) => <Text>°{item}</Text>}
+          data={data}
+          renderItem={({ item }) => (
+            <View style={estilos.lastItem}>
+              <Text style={estilos.bullet}>° </Text>
+              <Text style={estilos.text}>{item}</Text>
+            </View>
+            
+          )}
         />
     </View>
   )
 }
 
-const estuilos = StyleSheet.create({
+const estilos = StyleSheet.create({
     container: {
         marginTop: 40,
         padding: 20,
+    },
+    lastItem: {
+      flexDirection: "row",
+      allignItens: "center",
+      marginBottom: 8,
+      padding: 12,
+      backgroundColor: "#FFA500",
+      borderWidth: 1,
+      borderColor: "gray",
+      borderRadius: 10,
+      elevation: 3,
+    },
+    bullet: {
+      fontSize: 20,
+      marginRight: 8,
+    }, 
+    text: {
+      fontSize: 16,
     }
 })
